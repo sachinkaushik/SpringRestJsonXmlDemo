f65e1813875857f73aa20903b3b4c9a1f9be13d6
Sachin Sharma <sachin_shar@HCL.COM>
2018-01-03 15:42:40 +0530

    changed api

--- src/main/java/com/demo/controllers/TestController.java
+++ src/main/java/com/demo/controllers/TestController.java
@@ -9,9 +9,24 @@ import com.demo.model.Employee;
 @RestController
 public class TestController {
 
-	@RequestMapping(value = "/employee", method = RequestMethod.GET,
-			produces = { "application/json", "application/xml" })
-	public Employee firstPage() {
+	@RequestMapping(value = "/employee", method = RequestMethod.GET, produces = { "application/json", "application/xml" })
+	public Employee getEmployee() {
+
+		Employee emp = new Employee();
+		emp.setName("emp1");
+		emp.setDesignation("manager");
+		emp.setEmpId("1");
+		emp.setSalary(3000);
+		System.out.println(emp);
+		return emp;
+	}
+	
+	/**
+	 * you dont need to set produce as we set in above api
+	 * @return
+	 */
+	@RequestMapping(value = "/employees", method = {RequestMethod.GET, RequestMethod.PUT})
+	public Employee getEmployees() {
 
 		Employee emp = new Employee();
 		emp.setName("emp1");
